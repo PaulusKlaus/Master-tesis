650a7085a471c410e6b10f3b50e87e5720b965b9
Palina <122972254+PaulusKlaus@users.noreply.github.com>
2026-03-10 13:43:59 +0100

    JNU modifid

--- data_utils/datasets/JNU.py
+++ data_utils/datasets/JNU.py
@@ -169,7 +169,8 @@ class JNU(object):
             raise ValueError(f"Unknown split='{split}'. Use 'RA', 'R_NA', or 'O_A'.")
 
         # --- choose transforms ---
-        if split in ("RA", "O_A"):  # augmentation on train only
+        if split in ("RA", "O_A", "O_N"):  # augmentation on train only
+            print(" Augmentetion Hi")
             train_t1 = data_transforms(self.augmentation_1, self.normlizetype)
             train_t2 = data_transforms(self.augmentation_2, self.normlizetype)
         else:  # no augmentation
