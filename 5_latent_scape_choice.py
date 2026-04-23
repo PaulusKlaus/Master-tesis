fb42030b64a0df31761074c864a9e02ba783d9b2
Palina <122972254+PaulusKlaus@users.noreply.github.com>
2026-04-23 14:03:31 +0200

    correct hidden layer for the ls cwru

--- 5_latent_scape_choice.py
+++ 5_latent_scape_choice.py
@@ -11,8 +11,10 @@ def latent_space_test(paths, group_by_blocks=False):
 
 
 
-paths =["checkpoint/SSF_PU_0422-095246/training.log", #6-10 blocks
+paths =["checkpoint/SSF_PU_0422-095246/training.log", # hidden channel 160 
+        #"checkpoint/SSF_PU_0423-100558/training.log",  # hidden channel 128 
 
-       #"checkpoint/SSF_CWRU_0422-091220/training.log",
+       #"checkpoint/SSF_CWRU_0422-091220/training.log", # hidden channel 160 
+       "checkpoint/SSF_CWRU_0423-140218/training.log" # hidden channel 128
         ]
 latent_space_test(paths)
