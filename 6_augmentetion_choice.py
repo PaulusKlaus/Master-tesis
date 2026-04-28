a27ddccb9406ca11eba04b916303a017fe778afe
Palina <122972254+PaulusKlaus@users.noreply.github.com>
2026-04-28 08:42:55 +0200

    augmetation pu finished

--- 6_augmentetion_choice.py
+++ 6_augmentetion_choice.py
@@ -3,9 +3,9 @@
 from visualisation_training import aug_pair_vs_blocks_accuracy, augmentation_test
 
 
-paths =[#"checkpoint/SSF_PU_0427-144302/training.log", # no normalization, all augmentations 3 times 
+paths =["checkpoint/SSF_PU_0427-144302/training.log", # no normalization, all augmentations 3 times 
 
-       "checkpoint/SSF_PU_0422-095246/training.log",
+       #"checkpoint/SSF_PU_0422-095246/training.log",
         ]
 augmentation_test(paths)
 
